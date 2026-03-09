f3078011d6f6bb1d0c4c54920111399d5e62ee0c
Kamogelo Mokone <kamogelo.mokone@firsttech.digital>
2026-03-09 10:28:30 +0200

    Updates

--- _next/static/7Oe5VQWrT947pdbKikKPG/_buildManifest.js
+++ _next/static/7Oe5VQWrT947pdbKikKPG/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/MyPortfolio//_next/:path+",
+        "destination": "/MyPortfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
